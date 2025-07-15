4a02088621d2b289cf695da6cfc8cee371ae111b
TVATDCI <tvatdci@gmail.com>
2025-07-15 21:58:50 +0200

    getAllUsers, createUser, getUser, updateUser, deleteUser in progress

--- app.js
+++ app.js
@@ -166,6 +166,45 @@ const deleteTour = (req, res) => {
 //   });
 // });
 
+// #: getAllUsers
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'The route getAllUsers is in progress',
+  });
+};
+
+// #: getUser
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'The route getUser is in progress',
+  });
+};
+
+// #: createUser
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'The route is in progress',
+  });
+};
+
+// #: updateUser
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'The route createUser is in progress',
+  });
+};
+
+// #: deleteUser
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'The route deleteUser is in progress',
+  });
+};
 // app.get('/api/v1/tours', getAllTours);
 // app.get('/api/v1/tours/:id', getTour); // GET route to find a specific element by its ID in URLapp.post('/api/v1/tours', createTour); // Post Route to crate tour
 // app.patch('/api/v1/tours/:id', updateTour); // PATCH route to update a specific tour (for practice only)
