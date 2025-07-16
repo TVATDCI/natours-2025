703c92b7e8b8a61c1b111e783bc3ba66d9254554
TVATDCI <tvatdci@gmail.com>
2025-07-17 01:54:58 +0200

    param check tour:id, err 404

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,12 +5,9 @@ const tourController = require('../controllers/tourController');
 // logic: create routers for all routes and turn them into mini Express apps then mount them into the ROUTER below!
 const router = express.Router(); // modular router
 
-// Param MIDDLEWARES
-router.param('id', (req, res, next, val) => {
-  // val will keep the value of the id
-  console.log(`Param Middleware tour:ID is: ${val}`);
-  next();
-});
+// Param MIDDLEWARES to check the id
+// Register param middleware from tourController
+router.param('id', tourController.checkID);
 
 router
   .route('/') // root(/api/v1/tours)
