6b05b3d57d66e73de5ecf43358b9faa5acc39722
TVATDCI <tvatdci@gmail.com>
2025-07-16 22:40:32 +0200

    Object tourController.routes, option Object destructuring-DRY

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const tourController = require('./../controllers/tourController');
 
 // declare and define the Routers before mounting!
 // logic: create routers for all routes and turn them into mini Express apps then mount them into the ROUTER below!
@@ -6,13 +7,42 @@ const router = express.Router(); // modular router
 
 router
   .route('/') // root(/api/v1/tours)
-  .get(getAllTours) // Get all tours
-  .post(createTour); // Create a new tour
+  .get(tourController.getAllTours) // Get all tours
+  .post(tourController.createTour); // Create a new tour
 
 router
   .route('/:id')
-  .get(getTour) // Get a single tour by ID
-  .patch(updateTour) // Update a specific tour
-  .delete(deleteTour); // Delete a specific tour
+  .get(tourController.getTour) // Get a single tour by ID
+  .patch(tourController.updateTour) // Update a specific tour
+  .delete(tourController.deleteTour); // Delete a specific tour
 
 module.exports = router;
+
+/**
+ * Avoiding repeating tourController by destructuring Object method! DRY 
+ * I would!
+ * const express = require('express');
+const {
+  getAllTours,
+  createTour,
+  getTour,
+  updateTour,
+  deleteTour
+} = require('../controllers/tourController');
+
+const router = express.Router();
+
+router
+  .route('/')
+  .get(getAllTours)
+  .post(createTour);
+
+router
+  .route('/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
+
+module.exports = router;
+  
+ */
