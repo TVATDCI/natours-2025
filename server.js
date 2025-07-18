4efa11d1c883813be4441807e9d6582196962b94
TVATDCI <tvatdci@gmail.com>
2025-07-18 21:38:41 +0200

    set up port flexibility

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const dotenv = require('dotenv');
-const app = require('./app');
-
 dotenv.config({ path: './config.env' });
 
-// console.log(app.get('env'));
-console.log(process.env);
+const app = require('./app');
 
-const port = 3000;
+// console.log(app.get('env'));
+// console.log(process.env);
+// console.log('NODE_ENV:', process.env.NODE_ENV);
+// console.log('PORT:', process.env.PORT);
+// console.log('USERNAME:', process.env.DEV_USERNAME);
+// console.log('PASSWORD:', process.env.PASSWORD);
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
