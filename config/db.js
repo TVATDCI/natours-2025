76f60228eca893d95978b1a8cb4be1520a91e90f
TVATDCI <tvatdci@gmail.com>
2025-07-21 01:02:59 +0200

    DB connection successful

--- config/db.js
+++ config/db.js
@@ -4,7 +4,8 @@ const connectDB = async () => {
   try {
     const DB = await mongoose.connect(process.env.MONGO_URL);
     console.log(`MongoDB Connected: ${DB.connection.host}`);
-    console.log('DB connection successful!');
+    console.log(`Port: ${DB.connection.port}`);
+    console.log(`Database: ${DB.connection.name}`);
   } catch (error) {
     console.error(`MongoDB connection error: ${error.message}`);
     process.exit(1);
