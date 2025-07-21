66b7b9716d4cbe7d60f91ea06825f1762c25a6e5
TVATDCI <tvatdci@gmail.com>
2025-07-21 03:45:11 +0200

    create tourSchema, Tour model, test mongoose database

--- base/tourRoutesX.js
+++ base/tourRoutesX.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const express = require('express');
 
 const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
 );
 
 // #: getAllTours route
@@ -58,7 +58,7 @@ const createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
 
   // Merge the new ID with the incoming request body to create a new tour object
-  const newTour = Object.assign({ id: newId }, req.body);
+  const newTour = { id: newId, ...req.body };
 
   // Use .push to add the new tour to the in-memory tours array
   tours.push(newTour);
@@ -83,7 +83,7 @@ const createTour = (req, res) => {
           tour: newTour,
         },
       });
-    }
+    },
   );
 };
 
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const dotenv = require('dotenv');
 
+const mongoose = require('mongoose');
+
 dotenv.config({ path: './config.env' });
 
 const connectDB = require('./config/db');
@@ -8,6 +10,39 @@ const connectDB = require('./config/db');
 connectDB();
 
 const app = require('./app');
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: String,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Forest Hiker',
+  rating: 4.7,
+  price: 497,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('ERROR:', err);
+  });
 
 // console.log(app.get('env'));
 // console.log(process.env);
