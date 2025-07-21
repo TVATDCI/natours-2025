2605e26dc9e1d38586f4ec3069fbefa97f7bef9b
TVATDCI <tvatdci@gmail.com>
2025-07-21 14:56:54 +0200

    base/userControllerX.js

---
+++ base/userControllerX.js
@@ -0,0 +1,39 @@
+// #: getAllUsers
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'The route getAllUsers is in progress',
+  });
+};
+
+// #: getUser
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'The route getUser is in progress',
+  });
+};
+
+// #: createUser
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'The route createUser is in progress',
+  });
+};
+
+// #: updateUser
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'The route updateUser is in progress',
+  });
+};
+
+// #: deleteUser
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'The route deleteUser is in progress',
+  });
+};
