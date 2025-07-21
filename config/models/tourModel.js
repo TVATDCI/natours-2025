cf2751da317fea806ce46cfa4762adb7a013870d
TVATDCI <tvatdci@gmail.com>
2025-07-21 04:02:33 +0200

    create seeder script

--- config/models/tourModel.js
+++ config/models/tourModel.js
@@ -18,3 +18,21 @@
 
 // const Tour = mongoose.model('Tour', tourSchema);
 // module.exports = Tour;
+
+// Temporary seed script
+// const Tour = require('./models/tourModel');
+
+// const seed = async () => {
+//   try {
+//     const tour = await Tour.create({
+//       name: 'The Sea Explorer',
+//       rating: 4.8,
+//       price: '799',
+//     });
+//     console.log(tour);
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
+
+// seed();
