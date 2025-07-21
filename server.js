13715b098f41dffdf3448f7f1075e3f98a2ee863
TVATDCI <tvatdci@gmail.com>
2025-07-21 15:40:38 +0200

    refactor file-based mock data

--- server.js
+++ server.js
@@ -1,47 +1,12 @@
 const dotenv = require('dotenv');
-
-const mongoose = require('mongoose');
-
+// const mongoose = require('mongoose');
 const connectDB = require('./config/db');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-// Call the function to connect
+// Call the function from config/db to connect to mongoose database
 connectDB();
-
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: String,
-    required: [true, 'A tour must have a price'],
-  },
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The Forest Hiker',
-  rating: 4.7,
-  price: 497,
-});
-
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((err) => {
-    console.log('ERROR:', err);
-  });
 
 // console.log(app.get('env'));
 // console.log(process.env);
