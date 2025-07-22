3105a92d5bf4a2682417765345f9ab90c7bd1b86
TVATDCI <tvatdci@gmail.com>
2025-07-23 01:54:44 +0200

    implemented tourModel/tourSchema

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,18 +5,54 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true,
   },
-  tourName: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
     type: String,
+    required: [true, 'A tour must have a group size'],
   },
-  rating: {
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a group difficulty'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: {
+    type: Number,
+  },
+  summary: {
+    type: String,
+    required: [true, 'A tour must have a summary'],
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
