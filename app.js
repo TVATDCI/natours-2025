21df49f71dbe4978cd50c8f1efd9b137223c2b8d
TVATDCI <tvatdci@gmail.com>
2025-08-05 15:28:09 +0200

    Operational Error handler

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use('/api/v1/users', userRouter);
 app.all('*', (req, res, next) => {
   res.status(404).json({
     status: 'fail',
-    message: `Can't find ${RegExp.originalUrl} on this server!`,
+    message: `Can't find ${req.originalUrl} on this server!`,
   });
 });
 
