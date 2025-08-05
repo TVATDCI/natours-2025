9162a3a63ecbbd24ed00a75c2310436b7081c321
TVATDCI <tvatdci@gmail.com>
2025-08-05 17:24:18 +0200

    appError.js

---
+++ utils/appError.js
@@ -0,0 +1,14 @@
+// ======================================
+// appError.js
+// ======================================
+class AppError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+    this.isOperational = true; //  Marks this as an "expected" error
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
