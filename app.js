ac844d527d2b0d2c55bb9e9364321f964ec7be50
TVATDCI <tvatdci@gmail.com>
2025-08-05 17:28:44 +0200

    add console.log(err.stack) - stackTrace

--- app.js
+++ app.js
@@ -49,7 +49,8 @@ app.all('*', (req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-  console.log(err.stack);
+  // console.log(err.stack);
+
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'err';
 
