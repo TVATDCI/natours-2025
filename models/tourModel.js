3eb79a280471311a635d0a854b0f3898e443ad6d
TVATDCI <tvatdci@gmail.com>
2025-08-07 17:37:26 +0200

    ratingAverage

--- models/tourModel.js
+++ models/tourModel.js
@@ -40,8 +40,8 @@ const tourSchema = new mongoose.Schema(
     ratingsAverage: {
       type: Number,
       default: 4.5,
-      min: [1, 'Rating must must be above 1.0'],
-      max: [5, 'Rating must must be below 5.0'],
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
     },
     ratingsQuantity: {
       type: Number,
