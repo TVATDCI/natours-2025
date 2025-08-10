63acd095aab88f2f3bb91af5cf21a3fad58cda10
TVATDCI <tvatdci@gmail.com>
2025-08-10 17:43:21 +0200

    i jsonwebtoken

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
-//const crypto = require('crypto');
-//const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
