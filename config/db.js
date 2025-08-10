68564c239b22bec2ed5d0bdbe720ce58a5a26332
TVATDCI <tvatdci@gmail.com>
2025-08-10 20:49:18 +0200

    port-3000-27017

--- config/db.js
+++ config/db.js
@@ -13,7 +13,7 @@ const connectDB = async () => {
   // No try/catch — let the rejection bubble up
   const DB = await mongoose.connect(process.env.MONGO_URL);
   console.log(`MongoDB 🍃 connected successfully 🟢 : ${DB.connection.host}`);
-  console.log(`Port:🛂: ${DB.connection.port}`);
+  console.log(`Port:🌐: ${DB.connection.port}`);
   console.log(`Database: ${DB.connection.name}`);
 };
 
