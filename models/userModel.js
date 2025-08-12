4da4380837341277d03310d3eadf18a0db0cc18f
TVATDCI <tvatdci@gmail.com>
2025-08-12 20:24:53 +0200

    userSchema - changedPasswordAfter

--- models/userModel.js
+++ models/userModel.js
@@ -105,6 +105,8 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
       this.passwordChangedAt.getTime() / 1000,
       10,
     );
+
+    // console.log(this.passwordChangedAt, JWTTimestamp);
     return JWTTimestamp < changedTimestamp;
   }
 
