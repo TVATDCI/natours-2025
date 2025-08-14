650c306fa4c2ffaee33d917dff06f469f73ff7d0
TVATDCI <tvatdci@gmail.com>
2025-08-14 20:55:35 +0200

    test forgotPassword, email sending, resetPassword, and login - all verified

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,6 +5,8 @@ const User = require('../models/userModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
+const sendEmail = require('../utils/email');
+
 // ===============================
 // Helper: Create JWT Token
 // ===============================
@@ -250,20 +252,25 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   // 3) Build reset URL
   const resetURL = `${req.protocol}://${req.get('host')}/api/v1/users/resetPassword/${resetToken}`;
 
-  // 4) Send email (DEV: return token/URL in response instead of sending email)
+  // 4) Send the email
+  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email.`;
   try {
-    // TODO: replace with real email util
-    // await sendEmail({ email: user.email, subject: 'password reset token (valid 10 min)', message: `Reset here: ${resetURL}` });
+    await sendEmail({
+      email: user.email,
+      subject: 'Your password reset token (valid for 10 min)',
+      message,
+    });
 
     // DEV-ONLY: expose resetURL test in Postman
     res.status(200).json({
       status: 'success',
-      message: 'Token generated (DEV: see resetURL).',
+      message:
+        'Token generated (DEV: see resetURL) or copy resetTokenPlain. Go to resetPassword route and replace the resetToken in it.',
       resetURL, // <— remove in production
       resetTokenPlain: resetToken, // <— remove in production
     });
   } catch (err) {
-    // If email fails, clean fields
+    // Reset the token fields if email sending fails
     user.passwordResetToken = undefined;
     user.passwordResetExpires = undefined;
     await user.save({ validateBeforeSave: false });
