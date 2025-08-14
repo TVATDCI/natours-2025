6b93ed3cbce66724820fcd1fda8f9c2b1e5c9204
TVATDCI <tvatdci@gmail.com>
2025-08-15 01:17:52 +0200

    update role

--- controllers/authController.js
+++ controllers/authController.js
@@ -253,6 +253,7 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   const resetURL = `${req.protocol}://${req.get('host')}/api/v1/users/resetPassword/${resetToken}`;
 
   // 4) Send the email
+  // PROD-User Inbox
   const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email.`;
   try {
     await sendEmail({
