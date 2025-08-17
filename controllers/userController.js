f88f3a058ad2d6e8190e6773a4c55f4ada774568
TVATDCI <tvatdci@gmail.com>
2025-08-18 00:42:43 +0200

    status 204

--- controllers/userController.js
+++ controllers/userController.js
@@ -70,6 +70,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
 exports.deleteMe = catchAsync(async (req, res, next) => {
   await User.findByIdAndUpdate(req.user.id, { active: false });
 
+  // 204 deleted
   res.status(204).json({
     status: 'success',
     data: null,
