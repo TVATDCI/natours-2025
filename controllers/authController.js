0ac9709bbd1e0a9201e96422a8a9fc716dae4ed7
TVATDCI <tvatdci@gmail.com>
2025-08-17 22:41:25 +0200

    updatePassword logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -321,3 +321,24 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   // 6) Log the user in with a fresh JWT
   createSendToken(user, 200, res);
 });
+
+// ===============================
+// #: UPDATE CURRENT USER PASSWORD
+// ===============================
+exports.updatePassword = catchAsync(async (req, res, next) => {
+  // 1) Get current user from collection and ask for the password from protect middleware
+  const user = await User.findById(req.user.id).select('+password');
+
+  // 2) Check if POSTed current password is correct from `userSchema.methods.correctPassword`
+  if (!(await user.correctPassword(req.body.currentPassword, user.password))) {
+    return next(new AppError('Your current password is wrong.', 401));
+  }
+
+  // 3) If so, update password like in step 3 in resetPassword
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  await user.save(); // Use save(),runs the pre-save password hashing. NOT findByIdAndUpdate!
+
+  // 4) Log user in, send JWT
+  createSendToken(user, 200, res);
+});
