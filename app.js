5448616a6d4da331f269fcfd6f2d97903b3f1c84
TVATDCI <tvatdci@gmail.com>
2025-08-22 03:11:27 +0200

    implement reviewRouter

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const sanitizeHtmlMiddleware = require('./middleware/sanitizeHtml');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -97,6 +98,7 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 // Handle undefined routes
 app.all('*', (req, res, next) => {
