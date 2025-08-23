d6730d33b8dd4273f2caf199eacbc90efac6f6f1
TVATDCI <tvatdci@gmail.com>
2025-08-23 18:40:56 +0200

    add deleteReview route

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const reviewController = require('../controllers/reviewController');
 const authController = require('../controllers/authController');
 
-const router = express.Router({ mergeParams: true }); // merging param for both reviews from POST /tours, POST /reviews
+const router = express.Router({ mergeParams: true }); // Set to true merging param for both reviews from POST /tours, POST /reviews
 
 // Routes
 router
@@ -13,5 +13,7 @@ router
     authController.restrictTo('user'),
     reviewController.createReview,
   ); // = /api/v1/reviews → create a review (review, tour, user are required in (reviewModel)
+
+router.route('/:id').delete(reviewController.deleteReview);
 
 module.exports = router;
