80a8f93fe727c46303d7d02ca6c2dd40d0746ab6
TVATDCI <tvatdci@gmail.com>
2025-08-23 23:05:57 +0200

    refactor deleteUser by deleteOne

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,8 @@ const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
+const factory = require('./handlerFactory');
+
 // ===============================
 // #: GET ALL USERS
 // ===============================
@@ -150,15 +152,21 @@ exports.updateUser = catchAsync(async (req, res, next) => {
 // #: DELETE A USER
 // ===============================
 // DELETE /api/v1/users/:id
-exports.deleteUser = catchAsync(async (req, res, next) => {
-  const user = await User.findByIdAndDelete(req.params.id);
+// exports.deleteUser = catchAsync(async (req, res, next) => {
+//   const user = await User.findByIdAndDelete(req.params.id);
 
-  if (!user) {
-    return next(new AppError('No user found with that ID', 404));
-  }
+//   if (!user) {
+//     return next(new AppError('No user found with that ID', 404));
+//   }
 
-  res.status(204).json({
-    status: 'success',
-    data: null, // No content on delete
-  });
-});
+//   res.status(204).json({
+//     status: 'success',
+//     data: null, // No content on delete
+//   });
+// });
+// ===================================================
+// # deleteUser from deleteOne handlerFactory function
+// NOTE: Refactored Version from handlerFactory!
+
+exports.deleteUser = factory.deleteOne(User);
+// ===================================================
