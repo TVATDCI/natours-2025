eb38eecf1439f7a17410b88b9d25a838c6b4847f
TVATDCI <tvatdci@gmail.com>
2025-08-23 18:38:30 +0200

    import handlerFactory - create review to be solved

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,35 +1,35 @@
 const Review = require('../models/reviewModel');
 const catchAsync = require('../utils/catchAsync');
 
-// const factory = require('./handlerFactory');
+const factory = require('./handlerFactory');
 
-// exports.getAllReviews = factory.getAll(Review);
-// exports.getReview = factory.getOne(Review);
+exports.getAllReviews = factory.getAll(Review);
+exports.getReview = factory.getOne(Review);
 // exports.createReview = factory.createOne(Review);
-// exports.updateReview = factory.updateOne(Review);
-// exports.deleteReview = factory.deleteOne(Review);
+exports.updateReview = factory.updateOne(Review);
+exports.deleteReview = factory.deleteOne(Review);
 
-exports.getAllReviews = catchAsync(async (req, res, next) => {
-  // let filter = {};
-  // implementing filter for all review of each tour with tourId.
-  // Check (filter) if there is tourId in the req. Then, put the object into the filter
-  // if (req.params.tourId) filter = { tour: req.params.tourId };
+// exports.getAllReviews = catchAsync(async (req, res, next) => {
+// let filter = {};
+// implementing filter for all review of each tour with tourId.
+// Check (filter) if there is tourId in the req. Then, put the object into the filter
+// if (req.params.tourId) filter = { tour: req.params.tourId };
 
-  // Option:
-  // If the request came from /tours/:tourId/reviews, then only return reviews for that tour
-  // This way is more concise than declaring let filter = {} and updating later!
-  const filter = req.params.tourId ? { tour: req.params.tourId } : {};
+// Option:
+// If the request came from /tours/:tourId/reviews, then only return reviews for that tour
+// With ternary opt: This way is more concise than declaring let filter = {} and updating later!
+//   const filter = req.params.tourId ? { tour: req.params.tourId } : {};
 
-  const reviews = await Review.find(filter);
+//   const reviews = await Review.find(filter);
 
-  res.status(200).json({
-    status: 'success',
-    results: reviews.length,
-    data: {
-      reviews,
-    },
-  });
-});
+//   res.status(200).json({
+//     status: 'success',
+//     results: reviews.length,
+//     data: {
+//       reviews,
+//     },
+//   });
+// });
 
 // =================================================================
 exports.createReview = catchAsync(async (req, res, next) => {
@@ -47,13 +47,13 @@ exports.createReview = catchAsync(async (req, res, next) => {
   });
 });
 
-// ==================================================================
+// // ==================================================================
 
-// additional info for reviewWithTour info!
-exports.getReviewWithTour = catchAsync(async (req, res, next) => {
-  const review = await Review.findById(req.params.id).populate({
-    path: 'tour',
-    select: 'name',
-  });
-  res.status(200).json({ status: 'success', data: { review } });
-});
+// // additional info for reviewWithTour info!
+// exports.getReviewWithTour = catchAsync(async (req, res, next) => {
+//   const review = await Review.findById(req.params.id).populate({
+//     path: 'tour',
+//     select: 'name',
+//   });
+//   res.status(200).json({ status: 'success', data: { review } });
+// });
