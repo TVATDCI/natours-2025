15c39aa83851b1f46992372f0462ce40aeb83894
TVATDCI <tvatdci@gmail.com>
2025-08-29 00:48:10 +0200

    getTourWithin controller

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -175,3 +175,42 @@ exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+// ==================================================================
+// #: Controller function for “tours within radius”
+// ==================================================================
+// tours-within/:distance/center/:latlng/unit/:unit
+// GET /api/v1/tours/tours-within/100/center/34.111745,-118.113491/unit/mi
+
+exports.getToursWithin = catchAsync(async (req, res, next) => {
+  const { distance, latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(','); // centralize by splitting latlng("str") with a comma. Then destructure them
+  // CHECK!
+  if (!lat || !lng) {
+    return next(
+      new AppError(
+        'Please provide latitude and longitude in the format lat,lng.',
+        400,
+      ),
+    );
+  }
+
+  console.log(distance, lat, lng, unit);
+
+  // Radius of Earth: in miles or kilometers
+  //   const radius = unit === 'mi' ? distance / 3963.2 : distance / 6378.1;
+
+  //   const tours = await Tour.find({
+  //     startLocation: {
+  //       $geoWithin: { $centerSphere: [[lng, lat], radius] },
+  //     },
+  //   });
+
+  res.status(200).json({
+    status: 'success',
+    // results: tours.length,
+    // data: {
+    //   data: tours,
+    // },
+  });
+});
