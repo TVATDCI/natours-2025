98bb03ec55a45f3b109f682bb2ed35b13a876fcb
TVATDCI <tvatdci@gmail.com>
2025-09-03 20:31:59 +0200

    implement pug viewRoutes

---
+++ routes/viewRoutes.js
@@ -0,0 +1,10 @@
+// routes/viewRoutes.js
+const express = require('express');
+const viewController = require('../controllers/viewController');
+
+const router = express.Router();
+
+router.get('/', viewController.getOverview);
+router.get('/tour', viewController.getTour);
+
+module.exports = router;
