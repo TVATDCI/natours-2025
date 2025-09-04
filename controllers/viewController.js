b95d3666ff892d8ce280a6aae3b84b42fce1a9bf
TVATDCI <tvatdci@gmail.com>
2025-09-04 17:29:56 +0200

    move AppError outta populate chain

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -12,13 +12,13 @@ exports.getOverview = catchAsync(async (req, res, next) => {
 });
 
 exports.getTour = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findOne({ slug: req.params.slug });
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
 
   if (!tour) {
-    return next(new AppError('No tour found with that name', 404)).populate({
-      path: 'reviews',
-      fields: 'review rating user',
-    });
+    return next(new AppError('No tour found with that name', 404));
   }
 
   res.status(200).render('tour', {
