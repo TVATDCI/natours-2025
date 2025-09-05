0391bb6d25549388d8be4b47e36f0c453773f330
TVATDCI <tvatdci@gmail.com>
2025-09-06 00:27:11 +0200

    custom map tile

--- public/js/leaflet-map.js
+++ public/js/leaflet-map.js
@@ -30,6 +30,14 @@ if (mapEl) {
     className: 'leaflet-dark-theme',
   }).addTo(map);
 
+  // ore-styled map tiles
+  //   L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+  //     attribution:
+  //       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://www.carto.com/">CARTO</a>',
+  //     subdomains: 'abcd',
+  //     maxZoom: 20,
+  //   }).addTo(map);
+
   // Custom icon
   //   const customIcon = L.icon({
   //     iconUrl: '/img/pin.png',
