e7f3a600ab64ce4f42ac511c267074b245a65496
TVATDCI <tvatdci@gmail.com>
2025-09-06 00:43:37 +0200

    switch back to default map tile

--- public/js/leaflet-map.js
+++ public/js/leaflet-map.js
@@ -30,7 +30,7 @@ if (mapEl) {
     className: 'leaflet-dark-theme',
   }).addTo(map);
 
-  // ore-styled map tiles
+  // pre-styled map tiles
   //   L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
   //     attribution:
   //       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://www.carto.com/">CARTO</a>',
