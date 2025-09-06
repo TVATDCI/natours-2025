d80a57a235bc4ea04d0e38736ca688de3f9031c8
TVATDCI <tvatdci@gmail.com>
2025-09-06 22:32:29 +0200

    create login req form

---
+++ public/js/login.js
@@ -0,0 +1,32 @@
+/* eslint-disable */
+// import axios from 'axios';
+
+const login = async (email, password) => {
+  //   try {
+  //     const res = await axios({
+  //       method: 'POST',
+  //       url: '/api/v1/users/login',
+  //       data: { email, password },
+  //     });
+
+  //     if (res.data.status === 'success') {
+  //       alert('Logged in successfully!');
+  //       window.setTimeout(() => {
+  //         location.assign('/');
+  //       }, 1500);
+  //     }
+  //   } catch (err) {
+  //     alert(err.response.data.message);
+  //   }
+  alert(email, password);
+};
+
+// DOM handling
+const form = document.querySelector('.form');
+// if (form)
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  login(email, password);
+});
