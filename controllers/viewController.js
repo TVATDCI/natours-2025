f3841e05a4df76ebf9d2fc0743c0172c8f3a0e69
TVATDCI <tvatdci@gmail.com>
2025-09-06 22:40:33 +0200

    create /login route handler

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -32,39 +32,3 @@ exports.getLoginForm = (req, res) => {
     title: 'Log into your account',
   });
 };
-
-// ======================================
-// Mock Data (for testing only)
-// ======================================
-// const tours = [
-//   { name: 'The Forest Hiker', duration: 5, price: 497 },
-//   { name: 'The Sea Explorer', duration: 7, price: 997 },
-// ];
-
-// const tour = { name: 'The Forest Hiker', duration: 5, price: 497 };
-
-// ======================================
-// 3) ROUTES from app.js
-// ======================================
-
-// Root — for testing base layout directly
-// app.get('/', (req, res) => {
-//   const user = { name: 'TVATDCI' };
-//   res
-//     .status(200)
-//     .render('base', { user, tours, sampleTour: 'The Forest Hiker' });
-// });
-
-// // Overview — list of tours
-// app.get('/overview', (req, res) => {
-//   const user = { name: 'TVATDCI' };
-//   res.status(200).render('overview', { title: 'All Tours', tours, user });
-// });
-
-// // Tour — single tour
-// app.get('/tour', (req, res) => {
-//   const user = { name: 'TVATDCI' };
-//   res
-//     .status(200)
-//     .render('tour', { title: 'The Forest Hiker Tour', tour, user });
-// });
