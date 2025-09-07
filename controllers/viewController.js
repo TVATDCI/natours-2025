2720eb2775cc4b75fb96bb07d9d7572d7256aea8
TVATDCI <tvatdci@gmail.com>
2025-09-07 21:26:16 +0200

    getLoginForm: Add title Login Form

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -29,6 +29,6 @@ exports.getTour = catchAsync(async (req, res, next) => {
 
 exports.getLoginForm = (req, res) => {
   res.status(200).render('login', {
-    title: 'Log into your account',
+    title: 'Login Form',
   });
 };
