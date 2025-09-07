b818292d0acd8df6d6431f06b464a9ce8bf8b0e1
TVATDCI <tvatdci@gmail.com>
2025-09-07 18:46:08 +0200

    implement isLoggedIn middleware

--- controllers/authController.js
+++ controllers/authController.js
@@ -218,6 +218,41 @@ exports.protect = catchAsync(async (req, res, next) => {
   next();
 });
 
+// ===============================
+// #: isLoggedIn middleware - Only for rendered pages. NO ERRORS - No token in the header
+// ===============================
+exports.isLoggedIn = catchAsync(async (req, res, next) => {
+  // 1) Get token (from Authorization header or cookies)
+  try {
+    if (req.cookies.jwt) {
+      // 1) Verify token
+      const decoded = await promisify(jwt.verify)(
+        req.cookies.jwt,
+        process.env.JWT_SECRET,
+      );
+
+      // 2) Check if user still exists
+      const currentUser = await User.findById(decoded.id);
+      if (!currentUser) return next();
+
+      // 3) Check if user changed password after token was issued
+      console.log('Decoded JWT:', decoded);
+      console.log('Current user:', currentUser._id);
+      if (currentUser.changedPasswordAfter(decoded.iat)) {
+        return next();
+      }
+
+      // THERE IS A LOGGED IN USER - Render the currentUser as a locals.user!
+      res.locals.user = currentUser; // NOTE: makes user available - access in Pug templates
+      return next();
+    }
+  } catch (err) {
+    // just continue without crashing.
+    return next();
+  }
+  next();
+});
+
 // Logged in user & admin or lead-guide made it to this point
 // authController.protect, // must logged in
 // authController.restrictTo('admin', 'lead-guide'), // admin or lead-guide only
