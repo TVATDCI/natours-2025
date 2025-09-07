bd5a624e5e8a39987b540ea829b1463ecf7b0421
TVATDCI <tvatdci@gmail.com>
2025-09-07 17:39:20 +0200

    add req.cooies

--- app.js
+++ app.js
@@ -126,6 +126,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Debugging: attach request time + log headers
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
+  console.log(req.cookies);
   console.log(req.headers); // DEBUG
   next();
 });
