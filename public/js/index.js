aaadaa2741018602c791b209309ce7edbd92f797
TVATDCI <tvatdci@gmail.com>
2025-09-08 22:17:42 +0200

    reverse - PAUSE

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,18 @@
-console.log('Hello from index.js. Powered by PARCEL V2');
+/* eslint-disable */
+// import { login } from './login';
+
+// import { showAlert } from './login';
+
+// // DOM HANDLING
+// const form = document.querySelector('.form');
+// if (form) {
+//   form.addEventListener('submit', (e) => {
+//     e.preventDefault();
+
+//     const email = document.getElementById('email').value;
+//     const password = document.getElementById('password').value;
+
+//     login(email, password);
+//   });
+// }
+console.log('INDEX.JS');
