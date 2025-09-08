45995a2334f81cf4acd686d111d7af909e0b5d17
TVATDCI <tvatdci@gmail.com>
2025-09-08 16:55:41 +0200

    mapbox correction

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -7,47 +7,45 @@ console.log('Locations data', locations);
 mapbox.accessToken =
   'pk.eyIn2025=mapbox-ask-for-creditcardNumber-So-i-switchtoLeaflet.comAndcombineItWithStreetmap.com';
 
-  var map = new mapboxgl.Map({
-    container: 'map'({
-        style: 'mapbox://userName/_id',
-        scrollZoom: false,
-        // center: [-118.113491, 34.111745], // [lat, Lng]
-        // zoom: 10,
-        // interactive: false
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://userName/_id',
+  scrollZoom: false,
+  // center: [-118.113491, 34.111745], // [lat, Lng]
+});
 
-    });
+const bounds = new mapboxgl.LngLatBounds();
 
+locations.forEach((loc) => {
+  // create marker
+  const el = document.createElement('div');
+  el.className = 'marker';
 
-  const bounds = new mapboxgl.LngLatBounds();
-
-  locationbar.forEach(loc => {
-    // create marker
-    const el = document.createElement('div');
-    el.className = 'marker'
-
-    // add marker 
-    new mapboxgl.Marker({
-        element: el,
-        anchor: 'bottom'
-    })
-    setLngLat(loc.coordinates).addTo(map);
-
-    // Add pop up when a marker is clicked!
-    new mapboxgl.Popup({
-        offset: 30
-    }
-    ).setLngLat(loc.coordinates).setHTML(`<p>Day${loc.doc}: ${loc.description}</p>`)
-
-
-    // Extend map bounds to include current location
-    bounds.extend(loc.coordinates);
+  // add marker
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom',
   })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
 
-  map.fitBounds(bounds, {
-    padding: {
-        top: 200,
-        bottom: 120,
-        left: 100,
-        right: 100
-    }
+  // Add pop up when a marker is clicked!
+  new mapboxgl.Popup({
+    offset: 30,
   })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.doc}: ${loc.description}</p>`)
+    .addTo(map);
+
+  // Extend map bounds to include current location
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 120,
+    left: 100,
+    right: 100,
+  },
+});
