dabdc2a809cd139da2eb201d6a562d68990917a1
TVATDCI <tvatdci@gmail.com>
2025-09-09 00:39:59 +0200

    login with axios

--- public/js/login.js
+++ public/js/login.js
@@ -1,28 +1,18 @@
 /* eslint-disable */
-// import axios from 'axios';
+import axios from 'axios';
 
-// LOGIN HANDLER CDN version
+// LOGIN HANDLER AXIOS
 export const login = async (email, password) => {
   try {
-    const res = await axios({
-      method: 'POST',
-      url: '/api/v1/users/login',
-      data: { email, password },
-    });
+    const res = await axios.post('/api/v1/users/login', { email, password });
 
-    console.log('Login response:', res);
-
-    // Check the data.status. if === to success then redirect to homepage(/)
     if (res.data.status === 'success') {
-      alert('Logged in is very fucking successfully!');
-      window.setTimeout(() => {
-        location.assign('/'); // redirect to homepage
-      }, 1500);
+      alert('Logged in successfully!');
+      window.setTimeout(() => location.assign('/'), 1500);
     }
   } catch (err) {
-    console.error('Login error:', err.response || err);
     alert(
-      err.response.data.message || 'Something went wrong! Please try again.',
+      err.response?.data?.message || 'Something went wrong! Please try again.',
     );
   }
 };
