00bed306341c1c03057a5fa9192c8a7c241ea700
TVATDCI <tvatdci@gmail.com>
2025-09-09 01:07:21 +0200

    create login DOM, import js bundle

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,20 @@
 /* eslint-disable */
-// import { login } from './login';
+// public/js/index.js - Main Entry for JS Bundle
+// ADD Global JS
+// only export the function from login.js
+import { login } from './login.js';
+// 2) Import map.js (if used on certain pages)
+import { leaflet } from './leaflet.js';
+import { leafletMap } from './leafletMap';
 
-// import { showAlert } from './login';
+// PAGE-SPECIFIC: only run this if we're on the login page
+const form = document.querySelector('.form');
+if (form) {
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
 
-// // DOM HANDLING
-// const form = document.querySelector('.form');
-// if (form) {
-//   form.addEventListener('submit', (e) => {
-//     e.preventDefault();
-
-//     const email = document.getElementById('email').value;
-//     const password = document.getElementById('password').value;
-
-//     login(email, password);
-//   });
-// }
-console.log('INDEX.JS');
+    login(email, password);
+  });
+}
