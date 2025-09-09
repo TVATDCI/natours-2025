b4206fa7a5e8ad660ed4b5e4a29bf8f98c5a6848
TVATDCI <tvatdci@gmail.com>
2025-09-09 18:45:50 +0200

    bundle correction

--- public/js/index.js
+++ public/js/index.js
@@ -9,13 +9,13 @@ import { leaflet } from './leaflet.js';
 import { leafletMap } from './leafletMap';
 
 // DOM Handling: only run this if we're on the login page
-const form = document.querySelector('.form');
-if (form) {
-  form.addEventListener('submit', (e) => {
+const loginForm = document.querySelector('.form');
+
+if (loginForm)
+  loginForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
     login(email, password);
   });
-}
