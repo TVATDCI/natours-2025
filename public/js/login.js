4022089bc736f5f2f356f8a35e479f06af50b618
TVATDCI <tvatdci@gmail.com>
2025-09-09 18:44:54 +0200

    axios config obj

--- public/js/login.js
+++ public/js/login.js
@@ -5,17 +5,29 @@ import { showAlert } from './alerts.js';
 // LOGIN HANDLER AXIOS
 export const login = async (email, password) => {
   try {
-    const res = await axios.post('/api/v1/users/login', { email, password });
+    const res = await axios({
+      method: 'POST',
+      url: '/api/v1/users/login',
+      data: {
+        email,
+        password,
+      },
+    });
+
+    // console.log('Login response:', res);
+    console.log(
+      'Calling showAlert with:',
+      'success',
+      'Logged in successfully!',
+    );
 
     if (res.data.status === 'success') {
       showAlert('success', 'Logged in successfully!');
       window.setTimeout(() => location.assign('/'), 1500);
     }
   } catch (err) {
-    showAlert(
-      'error',
-      err.response?.data?.message || 'Something went wrong! Please try again.',
-    );
+    console.error('Login error:', err.response || err);
+    showAlert('error', err.response.data.message || 'Something went wrong!');
   }
 };
 
