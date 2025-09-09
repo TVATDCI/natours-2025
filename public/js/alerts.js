25664232a2fa97ad119da733a47dff78bffcda3c
TVATDCI <tvatdci@gmail.com>
2025-09-09 14:34:05 +0200

    styling showAlert

--- public/js/alerts.js
+++ public/js/alerts.js
@@ -1,15 +1,32 @@
+// export const hideAlert = () => {
+//   const el = document.querySelector('.alert');
+//   if (el) el.parentElement.removeChild(el);
+// };
+
+// // Show alert (success or error)
+// export const showAlert = (type, msg) => {
+//   hideAlert(); // remove any existing alerts first
+
+//   const markup = `<div class="alert alert--${type}">${msg}</div>`;
+//   document.querySelector('body').insertAdjacentHTML('afterbegin', markup);
+
+//   window.setTimeout(hideAlert, 5000);
+// };
+
+// =====================================
+// Function declaration, hoisted version
+// =====================================
+export function hideAlert() {
+  const el = document.querySelector('.alert');
+  if (el) el.parentElement.removeChild(el);
+}
+
 // Show alert (success or error)
-export const showAlert = (type, msg) => {
+export function showAlert(type, msg) {
   hideAlert(); // remove any existing alerts first
 
   const markup = `<div class="alert alert--${type}">${msg}</div>`;
   document.querySelector('body').insertAdjacentHTML('afterbegin', markup);
 
   window.setTimeout(hideAlert, 5000);
-};
-
-// Hide alert
-export const hideAlert = () => {
-  const el = document.querySelector('.alert');
-  if (el) el.parentElement.removeChild(el);
-};
+}
