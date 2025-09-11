dcd436e2e8d3670a426a047e161c3aed89de83b7
TVATDCI <tvatdci@gmail.com>
2025-09-11 19:37:59 +0200

    updateSettings

---
+++ public/js/updateSettings.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+
+import axios from 'axios';
+import { showAlert } from './alerts';
+
+export const updateSettings = async (data) => {
+  try {
+    const res = await axios({
+      method: 'PATCH',
+      url: '/api/v1/users/updateMe',
+      data,
+    });
+
+    if (res.data.status === 'success') {
+      showAlert('success', 'Data updated successfully!');
+    }
+  } catch (err) {
+    showAlert('error', err.response.data.message);
+  }
+};
