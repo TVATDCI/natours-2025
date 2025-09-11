73eb05a455b5ce218df7b8a7ec94e789f2468521
TVATDCI <tvatdci@gmail.com>
2025-09-11 21:43:07 +0200

    specify update data

--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -1,14 +1,19 @@
 /* eslint-disable */
+// Update user data settings form in account.pug - It must be implemented in index.js (bundle entry)
 
 import axios from 'axios';
 import { showAlert } from './alerts';
 
-export const updateSettings = async (data) => {
+export const updateData = async (name, email) => {
   try {
+    // http: req directly to API endpoint = /updateMe
     const res = await axios({
       method: 'PATCH',
       url: '/api/v1/users/updateMe',
-      data,
+      data: {
+        name,
+        email,
+      },
     });
 
     if (res.data.status === 'success') {
