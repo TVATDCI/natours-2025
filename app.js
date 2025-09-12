3e7846686c3a78fb6dd0780685699a5a534beac5
TVATDCI <tvatdci@gmail.com>
2025-09-12 23:48:20 +0200

    implement app.use(express.urlencoded)({ extended: true})

--- app.js
+++ app.js
@@ -84,6 +84,7 @@ app.use('/api', limiter);
 // ======================================
 // Body parser, reading data from body (limit payload to 10kb) to req.body
 app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 // ======================================
 // Cookie parser: parses cookies into req.cookies
 app.use(cookieParser());
