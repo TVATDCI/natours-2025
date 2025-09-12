d06b916beabed3b58a99d32592b0201cb4879e36
TVATDCI <tvatdci@gmail.com>
2025-09-12 23:54:14 +0200

    update userPasswordForm

--- public/js/index.js
+++ public/js/index.js
@@ -5,7 +5,7 @@ import '@babel/polyfill';
 // only export the function from login.js
 import { login, logout } from './login.js';
 // import the updateUserSettings
-import { updateData } from './updateSettings.js';
+import { updateSettings } from './updateSettings.js';
 // Import map.js (if used on certain pages)
 import { leaflet } from './leaflet.js';
 import { leafletMap } from './leafletMap';
@@ -15,6 +15,7 @@ const loginForm = document.querySelector('.form--login');
 const logoutBtn = document.querySelector('.nav__el--logout');
 // select user data form
 const userDataForm = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector('.form-user-password');
 
 console.log('💡 index.js loaded successfully');
 
@@ -44,5 +45,23 @@ if (userDataForm)
     // DEBUG: log form data
     console.log('📤 Form submit:', { name, email });
     // updateData(name, email) → function only accepts name + email - Nothing else!
-    updateData(name, email);
+    updateSettings({ name, email }, 'data');
   });
+
+// === UPDATE FORM === USER SETTINGS (PASSWORD) ==============
+if (userPasswordForm) {
+  userPasswordForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+
+    console.log('📤 Form submit new password:', {
+      passwordCurrent,
+      password,
+      passwordConfirm,
+    });
+
+    updateSettings({ passwordCurrent, password, passwordConfirm}, 'password')
+}};
