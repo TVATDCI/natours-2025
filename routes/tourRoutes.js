0741301a7ae0f324d89c4ed5700efbc6cd3435d7
TVATDCI <tvatdci@gmail.com>
2025-09-15 00:38:11 +0200

    create uploadTourImages, resizeTourImages routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -68,6 +68,8 @@ router
   .patch(
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
+    tourController.uploadTourImages,
+    tourController.resizeTourImages,
     tourController.updateTour,
   ) // Update a specific tour
   .delete(
