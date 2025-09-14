77e0c487385678415af7d750c9cf85cb024fd05a
TVATDCI <tvatdci@gmail.com>
2025-09-15 00:36:00 +0200

    create multi images uploader

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,9 +1,53 @@
+const multer = require('multer');
+const sharp = require('sharp');
 const Tour = require('../models/tourModel');
 // const APIFeatures = require('../utils/apiFeatures');
 
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
+// ========== Upload multi photos ==========
+const multerStorage = multer.memoryStorage(); // in memory(req.file.buffer)
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an image! Please upload only images.', 400), false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+// like uploading user photo but instead of upload.single, to upload.fields
+exports.uploadTourImages = upload.fields([
+  { name: 'imageCover', maxCount: 1 },
+  { name: 'images', maxCount: 3 },
+]);
+
+// upload.single('image');
+// upload.array('images', 5);
+
+// Middleware: resize UPLOADED Tour Images
+exports.resizeTourImages = async (req, res, next) => {
+  console.log(req.files);
+  //   if (!req.file) return next();
+
+  //   req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+  //   await sharp(req.file.buffer)
+  //     .resize(500, 500)
+  //     .toFormat('jpeg')
+  //     .jpeg({ quality: 90 })
+  //     .toFile(`public/img/users/${req.file.filename}`);
+
+  next();
+};
+
+// =========================================
 const factory = require('./handlerFactory');
 
 // ======================================
