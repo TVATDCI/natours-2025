eae41ec656d520c48938ea4ed75ee323ef5e257f
TVATDCI <tvatdci@gmail.com>
2025-09-16 22:45:26 +0200

    feature: forgotPassword passwordReset(restURL

--- controllers/authController.js
+++ controllers/authController.js
@@ -316,12 +316,14 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   await user.save({ validateBeforeSave: false }); // The password is not being changed yet
 
   // 3) Build reset URL
-  const resetURL = `${req.protocol}://${req.get('host')}/api/v1/users/resetPassword/${resetToken}`;
-
-  // 4) Send the email
-  // PROD-User Inbox
-  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email.`;
   try {
+    const resetURL = `${req.protocol}://${req.get('host')}/api/v1/users/resetPassword/${resetToken}`;
+    await new Email(user, resetURL).sendPasswordReset();
+
+    // 4) Send the email
+    // PROD-User Inbox
+    //   const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email.`;
+
     // await sendEmail({
     //   email: user.email,
     //   subject: 'Your password reset token (valid for 10 min)',
