26a9530dbc193948d98f3c360c08ed1432113938
TVATDCI <tvatdci@gmail.com>
2025-09-17 23:59:02 +0200

    feature: stripe debug book-tour route

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,4 +1,6 @@
 /* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
 
 const stripe = Stripe(
   'pk_test_51S8ObTDyxIiaci9uJAmlHH9dV7igZcsVsLzpv3lrMqjSBleJRD6rs5CjbkjItnNfMcaVmllCZvQ5G0UQPV1axBZ900RoDqJd42',
@@ -6,5 +8,7 @@ const stripe = Stripe(
 
 export const bookTour = async (tourId) => {
   // 1) Get checkout session from API
+  const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
+  console.log();
   // 2) Redirect to Stripe checkout page
 };
