5d4d7b15f3a16243a3f8bb94314a01df3de76659
TVATDCI <tvatdci@gmail.com>
2025-09-17 21:37:15 +0200

    feature: bookingRouter

--- app.js
+++ app.js
@@ -13,10 +13,11 @@ const hpp = require('hpp');
 const sanitizeQueryMiddleware = require('./middleware/sanitizeQuery');
 const sanitizeHtmlMiddleware = require('./middleware/sanitizeHtml');
 
-const viewRouter = require('./routes/viewRoutes');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const bookingRouter = require('./routes/bookingRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -140,6 +141,7 @@ app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/booking', bookingRouter);
 
 // Handle undefined routes
 app.all('*', (req, res, next) => {
