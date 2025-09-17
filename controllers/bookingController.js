eaa9a36085607c63d8985477ac49decdbbe02e91
TVATDCI <tvatdci@gmail.com>
2025-09-17 21:36:36 +0200

    feature: stripe

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,40 +1,40 @@
-// const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 // const AppError = require('../utils/appError');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 1) Get the currently booked tour
-  // const tour = await Tour.findById(req.params.tourId);
+  const tour = await Tour.findById(req.params.tourId);
 
   // 2) Create checkout session
-  //   const session = await stripe.checkout.sessions.create({
-  //     payment_method_types: ['card'],
-  //     mode: 'payment',
-  //     // user comes back here after payment
-  //     success_url: `${req.protocol}://${req.get('host')}/?tour=${
-  //       req.params.tourId
-  //     }&user=${req.user.id}&price=${tour.price}`,
-  //     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
-  //     customer_email: req.user.email,
-  //     client_reference_id: req.params.tourId,
-  //     line_items: [
-  //       {
-  //         price_data: {
-  //           currency: 'usd',
-  //           unit_amount: tour.price * 100, // cents
-  //           product_data: {
-  //             name: `${tour.name} Tour`,
-  //             description: tour.summary,
-  //             images: [
-  //               `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`,
-  //             ],
-  //           },
-  //         },
-  //         quantity: 1,
-  //       },
-  //     ],
-  //   });
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    mode: 'payment',
+    // user comes back here after payment
+    success_url: `${req.protocol}://${req.get('host')}/?tour=${
+      req.params.tourId
+    }&user=${req.user.id}&price=${tour.price}`,
+    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+    customer_email: req.user.email,
+    client_reference_id: req.params.tourId,
+    line_items: [
+      {
+        price_data: {
+          currency: 'usd',
+          unit_amount: tour.price * 100, // cents
+          product_data: {
+            name: `${tour.name} Tour`,
+            description: tour.summary,
+            images: [
+              `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`,
+            ],
+          },
+        },
+        quantity: 1,
+      },
+    ],
+  });
 
   // 3) Send session to client
   res.status(200).json({
