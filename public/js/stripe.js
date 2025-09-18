71f1adcb522d5b1772bac298cb0ab2e5f01ad87d
TVATDCI <tvatdci@gmail.com>
2025-09-18 18:44:32 +0200

    feature: Stripe - replace publish key str

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -2,7 +2,9 @@
 import axios from 'axios';
 import { showAlert } from './alerts';
 
-const stripe = Stripe(process.env.STRIPE_PUBLISHABLE_KEY);
+const stripe = Stripe(
+  'pk_test_51S8ObTDyxIiaci9uJAmlHH9dV7igZcsVsLzpv3lrMqjSBleJRD6rs5CjbkjItnNfMcaVmllCZvQ5G0UQPV1axBZ900RoDqJd42',
+);
 
 export const bookTour = async (tourId) => {
   if (typeof Stripe === 'undefined') {
@@ -10,8 +12,9 @@ export const bookTour = async (tourId) => {
     return;
   }
   try {
-    // 1) Get checkout session from API
+    // 1) Get checkout-session from the server (API endpoint)
     const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
+    console.log(session);
 
     // 2) Redirect to Stripe checkout page
     await stripe.redirectToCheckout({
