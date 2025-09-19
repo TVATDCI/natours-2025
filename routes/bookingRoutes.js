74055251c4587b640ee3f8a22d7b10c7af34551b
TVATDCI <tvatdci@gmail.com>
2025-09-19 22:41:45 +0200

    Feature: restrict admin routes

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -4,10 +4,13 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-router.get(
-  '/checkout-session/:tourId',
-  authController.protect,
-  bookingController.getCheckoutSession,
-);
+// Remove protect single route to protect all routes after this middleware (216)
+router.use(authController.protect);
+
+router.get('/checkout-session/:tourId', bookingController.getCheckoutSession);
+
+// restrict administration routes to admins and lead-guild
+
+router.use(authController.restrictTo('admin', 'lead-guide'));
 
 module.exports = router;
