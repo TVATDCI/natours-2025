309cc29377e2a74e7f03dff34390e89025558aaf
TVATDCI <tvatdci@gmail.com>
2025-09-19 22:47:34 +0200

    Feature: booking api admin only

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -13,4 +13,16 @@ router.get('/checkout-session/:tourId', bookingController.getCheckoutSession);
 
 router.use(authController.restrictTo('admin', 'lead-guide'));
 
+// These routes (in user account.pug) can be seen by admins and lead-guild only
+router
+  .route('/')
+  .get(bookingController.getAllBookings)
+  .post(bookingController.createBooking);
+
+router
+  .route('/:id')
+  .get(bookingController.getBooking)
+  .patch(bookingController.updateBooking)
+  .delete(bookingController.deleteBooking);
+
 module.exports = router;
