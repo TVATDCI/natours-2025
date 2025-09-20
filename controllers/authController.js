cc700542af06eb080f3f1632116c99d122a4e65e
TVATDCI <tvatdci@gmail.com>
2025-09-20 23:22:42 +0200

    Feature: refactor IsLoggedIn

--- controllers/authController.js
+++ controllers/authController.js
@@ -82,52 +82,32 @@ exports.logout = (req, res) => {
 // #: PROTECT - protect (all tours) middleware
 // ===========================================
 exports.protect = catchAsync(async (req, res, next) => {
-  // 1) Get token (from Authorization header or cookies)
   let token;
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
     token = req.headers.authorization.split(' ')[1];
-  } else if (req.cookies && req.cookies.jwt) {
+  } else if (req.cookies.jwt) {
     token = req.cookies.jwt;
   }
 
-  // DEBUG:
-  // console.log('Token:', token);
+  if (!token)
+    return next(new AppError('You are not logged in! Please log in.', 401));
 
-  if (!token) {
-    return next(
-      new AppError('You are not logged in! Please log in to get access.', 401),
-    );
-  }
-
-  // 2) Verify token (promisify to make it return a promise to the function)
-  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET); // signature(JWT_SECRET) needed to verify the token
-
-  // DEBUG:
-  // console.log('decoded', decoded);
-
-  // 3) Check if user still exists and the id is inside the payload!
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
   const currentUser = await User.findById(decoded.id);
-  // DEBUG:
-  // console.log('Current User:🪪:', currentUser);
-  if (!currentUser) {
-    return next(
-      new AppError('The user belonging to this token no longer exists.', 401),
-    );
-  }
 
-  // 4) Check if user changed password after token was issued
-  if (currentUser.changedPasswordAfter(decoded.iat)) {
+  if (!currentUser)
+    return next(new AppError('The user no longer exists.', 401));
+
+  if (currentUser.changedPasswordAfter(decoded.iat))
     return next(
       new AppError('User recently changed password! Please log in again.', 401),
     );
-  }
 
-  // 5) Grant access to PROTECTED ROUTE (tourRoutes)
-  req.user = currentUser; // For route handlers - It will be checked in restrictedTo !
-  res.locals.user = currentUser; // For views/templates
+  req.user = currentUser;
+  res.locals.user = currentUser;
   next();
 });
 
