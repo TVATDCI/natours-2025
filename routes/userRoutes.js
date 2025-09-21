7b205674ad8e48716f2ecf867cd859a2b0b7c29e
TVATDCI <tvatdci@gmail.com>
2025-09-21 22:18:56 +0200

    Feature: stagging/dev

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,16 +1,9 @@
 const express = require('express');
-// import multer for img upload
-// const multer = require('multer'); moved to userController.js
 
 // Import controller
 const authController = require('../controllers/authController');
 const userController = require('../controllers/userController');
 
-// upload images
-// const upload = multer({ dest: 'public/img/users' }); // to the file system, NOT in the database - moved to userController
-
-// declare and define the Routers before mounting!
-// logic: create routers for all routes and turn them into mini Express apps then mount them into the ROUTER below!
 const router = express.Router();
 
 // ===============================
@@ -26,10 +19,6 @@ router.patch('/resetPassword/:token', authController.resetPassword);
 
 // Protect all routes after this middleware
 router.use(authController.protect);
-// Authentication Middleware (authController.protect):
-// This middleware checks if the request contains a valid JWT token.
-// If the token is missing or invalid,  preventing unauthorized access to the routes.
-// Now "authController.protect" can be removed from this point.
 
 // route for password updates must be protected. So only current logged-in users can use it:
 router.patch(
@@ -75,32 +64,3 @@ router
   .delete(userController.deleteUser);
 
 module.exports = router;
-
-/**
- * Avoiding repeating tourController by destructuring Object method! DRY 
- * I would!
- * const express = require('express');
-const {
-  getAllUsers,
-  createUser,
-  getUser,
-  updateUser,
-  deleteUser
-} = require('../controllers/tourController');
-
-const router = express.Router();
-
-router
-  .route('/')
-  .get(getAllUsers)
-  .post(createUser);
-
-router
-  .route('/:id')
-  .get(getUser)
-  .patch(updateUser)
-  .delete(deleteUser);
-
-module.exports = router;
-  
- */
