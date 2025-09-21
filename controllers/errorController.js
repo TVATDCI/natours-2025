8b83619772555b2fed2c526698c21e2be84b9049
TVATDCI <tvatdci@gmail.com>
2025-09-21 17:31:54 +0200

    Feature: stagging cleaned-dev

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,54 +1,19 @@
-// ======================================
-// errorController.js / Global Error Handler
-// ======================================
-// 1. Imports
-// 2. Helper functions (like handleCastErrorDB)
-// 3. Error response functions (sendErrorDev / sendErrorProd)
-// 4. Exported middleware (main error handler)
-
 const AppError = require('../utils/appError');
 
 // =====================
 // Handling MongoDB Cast Error
 // =====================
-// 400 Bad Request for invalid ObjectId formats
-// MongoDB: Invalid ID (CastError)
-// In Production only.
 const handleCastErrorDB = (err) => {
   const message = `Invalid ${err.path}: ${err.value}.`;
 
   return new AppError(message, 400);
 };
 
-// NOTE: Switch to production
-
-// NOTE: error example: 400 Bad Request! from DB err
-// "status": "error",
-// "error": {
-//     "stringValue": "\"6885669311c1889fa57wwwww\"",
-//     "valueType": "string",
-//     "kind": "ObjectId",
-//     "value": "6885669311c1889fa57wwwww",
-//     "path": "_id",
-//     "reason": {},
-//     "name": "CastError",
-//     "message": "Cast to ObjectId failed for value \"6885669311c1889fa57wwwww\" (type string) at path \"_id\" for model \"Tour\""
-// },
-// NOTE: To 400 Bad Request! from AppError!
-// {
-//     "status": "fail",
-//     "message": "Invalid _id: 6885669311c1889fa57wwwww."
-// }
-
 // =======================================
 // Handling MongoDB duplicate field ERROR
 // =======================================
 
 const handleDuplicationFieldsDB = (err) => {
-  //   const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0]; // v.1 - Regex
-  //DEBUG: if the value of err.errmsg matches regEx as 1st index
-  // console.log(value);
-
   const field = Object.keys(err.keyValue)[0]; // v.2 access Mongoose err.keyValue directly (2025)
   const value = err.keyValue[field]; // Using **err.keyValue** is more reliable and cleaner
 
@@ -62,15 +27,13 @@ const handleDuplicationFieldsDB = (err) => {
 
 const handleValidationErrorDB = (err) => {
   const errors = Object.values(err.errors).map((el) => el.message);
-  const message = `Invalid input data: ${errors.join('. ')}`; // Join the messages together with .(dot)_(space)
+  const message = `Invalid input data: ${errors.join('. ')}`;
   return new AppError(message, 400);
 };
 
 // =================================
 // Handling invalid JWT ERROR
 // =================================
-// BTW. ES6 arrow function - use an implicit return to simplify arrow function by removing the curly braces and the return keyword!
-// REASON: Curly braces + return are only needed if your function body has multiple statements.
 const handleJWTError = () =>
   new AppError('Invalid token. Please log in again.', 401);
 
@@ -118,8 +81,6 @@ const sendErrorProd = (err, req, res) => {
       });
     }
 
-    // Programming or Unknown errors → Don't leak details
-    // Log error
     console.error('ERROR 🧨', err);
 
     // send generic message
@@ -159,9 +120,6 @@ module.exports = (err, req, res, next) => {
   // DEVELOPMENT MODE: send full error details
   // ======================================
   if (process.env.NODE_ENV === 'development') {
-    // DEBUG: in development if you are too lazy to switch to :prod
-    // tracking err.name CastError for during dev amd hardcoded to bad request!
-    if (err.name === 'CastError') err.statusCode = 400;
     return sendErrorDev(err, req, res);
   }
 
@@ -169,11 +127,7 @@ module.exports = (err, req, res, next) => {
   // PRODUCTION MODE: avoid leaking sensitive info
   // ======================================
   if (process.env.NODE_ENV === 'production') {
-    let error = { ...err };
-
-    // error.message preserve message before the error gets transformed or doesn't
-    error.message = err.message; // session: 193 min: 19
-    // Avoids 'undefined' messages or missing logs in transformed errors.
+    let error = { ...err, message: err.message };
 
     // =============================================
     // Handle known operational error types with custom messages
@@ -200,9 +154,7 @@ module.exports = (err, req, res, next) => {
     return sendErrorProd(error, req, res);
   }
 
-  // ======================================
-  // DEFAULT FALLBACK (safety net for misconfigured NODE_ENV)
-  // ======================================
+  // Fallback for misconfigured NODE_ENV
   res.status(err.statusCode).json({
     status: err.status,
     message: 'An unexpected error occurred.',
