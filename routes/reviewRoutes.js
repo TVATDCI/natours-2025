06650cfba6eefa134c6b9286a4364e7d816febdc
TVATDCI <tvatdci@gmail.com>
2025-09-21 22:12:00 +0200

    Feature: stagging/dev

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const reviewController = require('../controllers/reviewController');
 const authController = require('../controllers/authController');
 
-const router = express.Router({ mergeParams: true }); // Set to true merging param for both reviews from POST /tours, POST /reviews
+const router = express.Router({ mergeParams: true });
 
 // Protect all routes after this middleware
 router.use(authController.protect);
@@ -15,19 +15,17 @@ router
     authController.restrictTo('user'),
     reviewController.setTourUserIds,
     reviewController.createReview,
-  ); // = /api/v1/reviews → create a review (review, tour, user are required in (reviewModel)
+  );
 
 router
   .route('/:id')
   .get(reviewController.getReview)
   .patch(
     authController.restrictTo('user', 'admin'),
-    // reviewController.checkReviewOwnership,
     reviewController.updateReview,
   )
   .delete(
     authController.restrictTo('user', 'admin'),
-    // reviewController.checkReviewOwnership,
     reviewController.deleteReview,
   );
 
