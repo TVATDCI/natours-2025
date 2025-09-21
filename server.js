16521928d1be067fbcb787e5b4c473dbc7f4e77c
TVATDCI <tvatdci@gmail.com>
2025-09-21 22:29:34 +0200

    Feature: stagging/dev

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
+/* eslint-disable */
 const dotenv = require('dotenv');
 
-// throw new Error('🧨 Test uncaughtException!');
-// ======================================
-// Global Uncaught Exception Handler - listen to events even before requiring main app
-// (Synchronous errors outside Express)
-// Keep uncaughtException → protects against sync runtime errors before/after Express is running.
 process.on('uncaughtException', (err) => {
   console.error('🧨 :UNCAUGHT EXCEPTION! Shutting down...');
   console.error(err);
@@ -16,11 +12,9 @@ dotenv.config({ path: './config.env' });
 const connectDB = require('./config/db');
 const app = require('./app');
 
-// ======================================
 // Connect to Database
 connectDB(); // No catch here — failures go to unhandledRejection
 
-// ======================================
 // Start server
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
