0005722bb944a60afccc414868492b9696c6a99a
TVATDCI <tvatdci@gmail.com>
2025-09-21 21:56:52 +0200

    Feature: stagging/dev

--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -1,15 +1,10 @@
 /* eslint-disable */
-// Update user data settings form in account.pug - It must be implemented in index.js (bundle entry)
-
 import axios from 'axios';
 import { showAlert } from './alerts';
 
-// ======================================================================================================================
+// ==========================================================================================================================
 // updateData V2. This version (updateSettings) will use ternary operator to differ url between updateData and updatePassword
-// NOTE: updateMe and updateMyPassword are 2 different endpoint, which mean it could be done separately!
-// ======================================================================================================================
-// type is either 'password' or 'data'
-// Call for updateSettings in js/index.js
+// ==========================================================================================================================
 export const updateSettings = async (data, type) => {
   try {
     const url =
@@ -25,7 +20,6 @@ export const updateSettings = async (data, type) => {
 
     // console.log('✅ Response from /updateMe:', res);
     // console.log('✅ Response from /updateMyPassword:', res);
-
     if (res.data.status === 'success') {
       showAlert('success', `${type.toUpperCase()} updated successfully!`);
 
@@ -33,7 +27,7 @@ export const updateSettings = async (data, type) => {
       if (type === 'data') {
         window.setTimeout(() => {
           location.reload();
-        }, 1500); // wait 1.5s so user can see success alert
+        }, 1500);
       }
     }
   } catch (err) {
@@ -41,33 +35,3 @@ export const updateSettings = async (data, type) => {
     showAlert('error', err.response?.data?.message || 'Update failed!');
   }
 };
-
-// ==========================================================================
-// updateData V1. This version is only specify updating Data {name and email}
-// Call for updateData in js/index.js
-// ==========================================================================
-// export const updateData = async (name, email) => {
-//   try {
-//     // DEBUG:
-//     console.log('📤 Sending PATCH to /updateMe:', { name, email });
-//     // http: req directly to API endpoint = /updateMe
-//     const res = await axios({
-//       method: 'PATCH',
-//       url: '/api/v1/users/updateMe',
-//       // The data is explicitly sent only name and email for the updating process
-//       data: {
-//         name,
-//         email,
-//       },
-//     });
-
-//     console.log('✅ Response from /updateMe:', res);
-
-//     if (res.data.status === 'success') {
-//       showAlert('success', 'Data updated successfully!');
-//     }
-//   } catch (err) {
-//     console.error('🔴 Update error:', err.response?.data || err.message);
-//     showAlert('error', err.response?.data?.message || 'Update failed!');
-//   }
-// };
