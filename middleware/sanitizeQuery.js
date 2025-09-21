ac6173314538b5cd0fa51a0510b7cc8bb1e28bfc
TVATDCI <tvatdci@gmail.com>
2025-09-21 22:09:56 +0200

    Feature: stagging/dev

--- middleware/sanitizeQuery.js
+++ middleware/sanitizeQuery.js
@@ -47,26 +47,3 @@ module.exports = (req, res, next) => {
 
   next();
 };
-
-// module.exports = (req, res, next) => {
-//   Object.entries(req.query).forEach(([key, value]) => {
-//     if (typeof value === 'string') {
-//       let cleaned = value.trim();
-
-//       // Optional: lowercase normalization for certain fields
-//       if (['difficulty', 'sort', 'order'].includes(key)) {
-//         cleaned = cleaned.toLowerCase();
-//       }
-
-//       // Convert to number if it's a numeric string
-//       const num = Number(cleaned);
-//       if (!Number.isNaN(num) && cleaned !== '') {
-//         cleaned = num;
-//       }
-
-//       req.query[key] = cleaned;
-//     }
-//   });
-
-//   next();
-// };
