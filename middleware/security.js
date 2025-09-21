12e5ccf58bc3fbbc6b902c2bc9ee824b7a166219
TVATDCI <tvatdci@gmail.com>
2025-09-22 00:55:19 +0200

    Feature: security & sanitization middlewares

---
+++ middleware/security.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const hpp = require('hpp');
+const sanitizeQueryMiddleware = require('./sanitizeQuery');
+const sanitizeHtmlMiddleware = require('./sanitizeHtml');
+
+const securityMiddleware = express.Router();
+
+securityMiddleware.use(express.json({ limit: '10kb' }));
+securityMiddleware.use(express.urlencoded({ extended: true, limit: '10kb' }));
+securityMiddleware.use(cookieParser());
+securityMiddleware.use(mongoSanitize());
+securityMiddleware.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+      'rating',
+    ],
+  }),
+);
+securityMiddleware.use(sanitizeQueryMiddleware);
+securityMiddleware.use(sanitizeHtmlMiddleware);
+
+module.exports = securityMiddleware;
