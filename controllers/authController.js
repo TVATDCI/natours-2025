65c7086cc0da4f65f2058f213aa10e2e4d58c407
TVATDCI <tvatdci@gmail.com>
2025-09-21 17:08:32 +0200

    Feature: stagging cleaned-dev

--- controllers/authController.js
+++ controllers/authController.js
@@ -49,6 +49,7 @@ exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create(req.body);
 
   const url = `${req.protocol}://${req.get('host')}/me`;
+  // console.log(`URL:📧: ${url}`);
   await new Email(newUser, url).sendWelcome();
 
   createSendToken(newUser, 201, res);
