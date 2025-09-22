4b486376fd546f3bd8786e72f12b766336c29e14
TVATDCI <tvatdci@gmail.com>
2025-09-22 15:23:12 +0200

    update workbench

--- app.js
+++ app.js
@@ -75,6 +75,7 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
 app.use('/api/v1/bookings', bookingRouter);
+
 // =======================
 // Handle undefined routes
 // =======================
