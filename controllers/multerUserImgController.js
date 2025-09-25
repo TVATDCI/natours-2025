5fe1c3098fb844796c8c030c34b947a43a20f7e6
TVATDCI <tvatdci@gmail.com>
2025-09-26 01:32:42 +0200

    multerUserImgController: comment remove

--- controllers/multerUserImgController.js
+++ controllers/multerUserImgController.js
@@ -57,18 +57,3 @@ exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
 
   next();
 });
-
-// Middleware: resize photo
-// exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
-//   if (!req.file) return next();
-
-//   req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
-
-//   await sharp(req.file.buffer)
-//     .resize(500, 500)
-//     .toFormat('jpeg')
-//     .jpeg({ quality: 90 })
-//     .toFile(`public/img/users/${req.file.filename}`);
-
-//   next();
-// });
