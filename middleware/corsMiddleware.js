80833ed25bffa37d4a3eb38b27e8e82402e478ba
TVATDCI <tvatdci@gmail.com>
2025-09-28 18:02:53 +0200

    cors issue-1

--- middleware/corsMiddleware.js
+++ middleware/corsMiddleware.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 // So different frontend URLs in development and production
 const allowedOrigins = [
   'http://localhost:3000',
+  'http://127.0.0.1:3000',
   process.env.FRONTEND_URL,
   'https://example.com',
 ].filter(Boolean);
