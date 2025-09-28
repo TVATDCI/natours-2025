384f50f72589629ce36d2ee2d13e947f8002e601
TVATDCI <tvatdci@gmail.com>
2025-09-29 01:46:04 +0200

    remove bookingController

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -6,12 +6,7 @@ const authController = require('../controllers/authController');
 const router = express.Router();
 
 // Overview and tour detail pages: visible to everyone, but show different header if logged in
-router.get(
-  '/',
-  //
-  authController.isLoggedIn,
-  viewController.getOverview,
-);
+router.get('/', authController.isLoggedIn, viewController.getOverview);
 router.get('/tours/:slug', authController.isLoggedIn, viewController.getTour);
 
 // Login form: if already logged in, header will reflect it
