44e51174c426f8aa94da2f8f47601cf4df913daa
TVATDCI <tvatdci@gmail.com>
2025-09-29 01:57:31 +0200

    update webhookCheckout

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,4 +1,4 @@
-const stripe = require('stripe')(process.env.STRIPE_TEST_KEY); // Must be on the top prior!
+const stripe = require('stripe')(process.env.STRIPE_TEST_KEY);
 // https://docs.stripe.com/webhooks
 
 const Tour = require('../models/tourModel');
@@ -41,41 +41,6 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
       },
     ],
   });
-  //     payment_method_types: ['card'],
-  //     mode: 'payment',
-  //     // user comes back here after payment with option cancel(cancel_url)
-  //     success_url: `${req.protocol}://${req.get('host')}/?tour=${
-  //       req.params.tourId
-  //     }&user=${req.user.id}&price=${tour.price}`,
-  //     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
-  //     customer_email: req.user.email,
-  //     client_reference_id: req.params.tourId,
-  //     // Product infos
-  //     line_items: [
-  //       {
-  //         price_data: {
-  //           currency: 'usd',
-  //           unit_amount: tour.price * 100, // cents
-  //           product_data: {
-  //             name: `${tour.name} Tour`,
-  //             description: tour.summary,
-  //             images: [
-  //               `https://www.natours.dev/img/tours/${tour.imageCover}`, //   `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`,
-  //             ],
-  //           },
-  //         },
-  //         quantity: 1,
-  //       },
-  //     ],
-  //   });
-
-  // check success_url being sent to Stripe:
-  //   console.log(
-  //     'Generated success_url:',
-  //     `${req.protocol}://${req.get('host')}/?tour=${
-  //       req.params.tourId
-  //     }&user=${req.user.id}&price=${tour.price}`,
-  //   );
 
   // 3) Send session to client
   res.status(200).json({
@@ -91,31 +56,6 @@ const createBookingCheckout = async (session) => {
   const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
 };
-
-// TEMP: Create INSECURE booking middle without STRIPE WEBHOOK (HACK)
-// exports.createBookingCheckout = catchAsync(async (req, res, next) => {
-//   // console.log('🎯 createBookingCheckout middleware HIT');
-//   // console.log('🔎 Full req.url:', req.url);
-//   // console.log('🔎 Full req.query:', req.query);
-
-//   const { tour, user, price } = req.query; // as in bookingModel.js
-
-//   if (!tour || !user || !price) {
-//     // console.log('⚠️ Missing query params:', { tour, user, price });
-//     return next();
-//   }
-
-//   console.log('Booking.create payload:', { tour, user, price });
-
-//   try {
-//     await Booking.create({ tour, user, price: +price }); // price = number
-//     // console.log('✅ Booking created');
-//   } catch (err) {
-//     console.error('❌ Booking failed:', err);
-//   }
-
-//   // More secure - Redirect to remove query params from URL
-//   // `${req.protocol}://${req.get('host')}/?tour
 //   res.redirect(req.originalUrl.split('?')[0]); // [0] = root url '/'
 // });
 
