2ea067f894bbd46ff46dd81dcb4e6f57d0ad0a14
TVATDCI <tvatdci@gmail.com>
2025-10-01 19:01:16 +0200

    implement show alerts globally

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -8,7 +8,7 @@ exports.alerts = (req, res, next) => {
   const { alert } = req.query;
   if (alert === 'booking')
     res.locals.alert =
-      'Your booking was successful! Please check your email for a confirmation. If your booking does not show up here immediately, pleaseY come back later.';
+      'Booking successful! Please check your email for a confirmation. Note: If your booking does not show up here immediately, please come back later.';
 
   next();
 };
