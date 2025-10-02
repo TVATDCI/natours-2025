c17cffd57d28d0edd0ba46fdc998eadef515987d
TVATDCI <tvatdci@gmail.com>
2025-10-02 02:18:30 +0200

    remove logs and debugging fields

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -5,15 +5,11 @@ const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
 exports.alerts = (req, res, next) => {
-  console.log('🔎 alerts middleware triggered');
-  console.log('   req.originalUrl:', req.originalUrl);
-  console.log('   req.url:', req.url);
-  console.log('   req.query:', req.query);
   const { alert } = req.query;
   if (alert === 'booking') {
     res.locals.alert =
       'Booking successful! Please check your email for a confirmation. Note: If your booking does not show up here immediately, please come back later.';
-    console.log('✅ res.locals.alert set:', res.locals.alert);
+    // console.log('✅ res.locals.alert set:', res.locals.alert);
   }
 
   next();
